f0239b1de9d40bdc66144ed11ce7b416ad3e510c
Kien Duy <buikienduy2020@gmail.com>
2025-01-03 20:50:43 +0700

    init user

---
+++ app/src/main/java/com/example/jobapp_u/home/fragments/AlertFragment.kt
@@ -0,0 +1,63 @@
+package com.example.jobapp_u.home.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.jobapp_u.databinding.FragmentAlertBinding
+import com.example.jobapp_u.home.adapter.AlertAdapter
+import com.example.jobapp_u.home.viewmodel.AlertViewModel
+import com.example.jobapp_u.util.Status.*
+import com.example.jobapp_u.util.showToast
+
+class AlertFragment : Fragment() {
+    private var _binding : FragmentAlertBinding? = null
+    private val binding get() = _binding!!
+    private var _alertAdapter : AlertAdapter? = null
+    private val alertAdapter get() = _alertAdapter!!
+    private val alertViewModel by viewModels<AlertViewModel>()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAlertBinding.inflate(inflater, container, false)
+        _alertAdapter = AlertAdapter()
+
+        setupUI()
+        setupObserver()
+
+        return binding.root
+    }
+
+    private fun setupUI() {
+        binding.apply {
+            alertViewModel.fetchNotifications()
+            rvNotification.adapter = alertAdapter
+            rvNotification.layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    private fun setupObserver() {
+        alertViewModel.notificationStatus.observe(viewLifecycleOwner){ notificationState ->
+            when(notificationState.status){
+                LOADING -> Unit
+                SUCCESS -> {
+                    val notifications = notificationState.data!!
+                    alertAdapter.setData(notifications)
+                }
+                ERROR -> {
+                    val errorMessage = notificationState.message!!
+                    showToast(requireContext(), errorMessage)
+                }
+            }
+        }
+    }
+    override fun onDestroyView() {
+        _binding = null
+        _alertAdapter = null
+        super.onDestroyView()
+    }
+}
